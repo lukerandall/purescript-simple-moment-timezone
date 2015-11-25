59b2c9184ce8b66a15a76a9214a94a8e61d92893
Luke Randall <luke.randall@gmail.com>
2015-11-25 21:55:36 +0200

    Add documentation

---
+++ docs/Data/Moment/Simple/Timezone.md
@@ -0,0 +1,21 @@
+## Module Data.Moment.Simple.Timezone
+
+A simple PureScript wrapper around moment-timezone.js
+
+#### `tz`
+
+``` purescript
+tz :: Moment -> Timezone -> Moment
+```
+
+Convert a Moment to the given timezone
+
+#### `tzFormat`
+
+``` purescript
+tzFormat :: Moment -> Timezone -> String -> String
+```
+
+Convert a Moment to the given timezone and format with the given string
+
+
--- src/Data/Moment/Simple/Timezone.purs
+++ src/Data/Moment/Simple/Timezone.purs
@@ -15,10 +15,12 @@ type Timezone = String
 
 foreign import tz_ :: Fn2 Moment Timezone Moment
 
+-- | Convert a Moment to the given timezone
 tz :: Moment -> Timezone -> Moment
 tz m t = runFn2 tz_ (clone m) t
 
 foreign import tzFormat_ :: Fn3 Moment Timezone String String
 
+-- | Convert a Moment to the given timezone and format with the given string
 tzFormat :: Moment -> Timezone -> String -> String
 tzFormat m t f = runFn3 tzFormat_ (clone m) t f
